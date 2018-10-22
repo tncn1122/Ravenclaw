adc9835b603edee0597aa26e5f160f37040c46b5
William <tncnhan@gmail.com>
2018-10-22 09:48:49 +0700

    Update template

---
+++ Note/template.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define  Input        	freopen("input.txt", "r", stdin)
+#define  Output			freopen("input.txt", "w", stdout)
+#define  fu(i,a,b) 		for(int i=(a); i<=(b); ++i)
+#define  fd(i,a,b) 		for(int i=(a); i>=(b); --i)
+#define  fo(i,n) 		for(int i=0; i<(n); ++i)
+#define  fix_set(x)     fixed<<setprecision(x)
+#define  ms(a,x)    	memset(a, x, sizeof(a))
+#define  all(x)         (x).begin(), (x).end()
+#define	 fst			first
+#define	 snd			second
+#define  allarr(x,n)	(x, x+n)
+
+
+typedef double db;
+typedef long long ll;
+ll const MAX = 1e18;
+ll const Base = 1;
+
+inline ll fpow(ll a,ll x) {if(x==0)return 1;	if(x&1) {return a*fpow(a,x-1)%Base;} else{ll t=fpow(a,x/2);return t*t%Base;}}  //tinh (a^x) % Base
+inline ll diMod(ll A, ll B){ll result = (A * fpow(B, Base-2)) % Base; return result;};		//tinh (A/B) % Base
+
+
+
+
+//declare
+
+
+//============//
+
+void solve()
+{
+
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	//Input;
+	//Output;
+	solve();
+	
+	return 0;
+}
