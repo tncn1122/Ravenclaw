b88ffbd098ea3f70adae8a886cdceb11b3c8430f
William <tncnhan@gmail.com>
2018-11-15 22:34:57 +0700

    Update template

--- Note/Template/template.cpp
+++ Note/Template/template.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define  Input        	freopen("input.txt", "r", stdin)
-#define  Output			freopen("output.txt", "w", stdout)
+#define  Infile        	freopen("input.txt", "r", stdin)
+#define  Outfile		freopen("output.txt", "w", stdout)
 #define  fu(i,a,b) 		for(int i=(a); i<=(b); ++i)
 #define  fd(i,a,b) 		for(int i=(a); i>=(b); --i)
 #define  fo(i,n) 		for(int i=0; i<(n); ++i)
@@ -16,12 +16,14 @@ using namespace std;
 
 typedef double db;
 typedef long long ll;
+clock_t TimeBegin, TimeEnd;
 ll const MAX = 1e18;
 ll const Base = 1;
 
 inline ll fpow(ll a,ll x) {if(x==0)return 1;	if(x&1) {return a*fpow(a,x-1)%Base;} else{ll t=fpow(a,x/2);return t*t%Base;}}  //tinh (a^x) % Base
-inline ll diMod(ll A, ll B){ll result = (A * fpow(B, Base-2)) % Base; return result;};		//tinh (A/B) % Base
-
+inline ll diMod(ll A, ll B){ll result = (A * fpow(B, Base-2)) % Base; return result;}		//tinh (A/B) % Base
+inline void Tstart();
+inline void Tstop();
 
 
 
@@ -30,7 +32,12 @@ inline ll diMod(ll A, ll B){ll result = (A * fpow(B, Base-2)) % Base; return res
 
 //============//
 
-void solve()
+void Input()
+{
+	
+}
+
+void Solve()
 {
 
 }
@@ -38,9 +45,21 @@ void solve()
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	//Input;
-	//Output;
-	solve();
-	
+	//Infile;
+	//Outfile;
+	Input();	Tstart();
+	Solve();
+				Tstop();	
 	return 0;
+}
+
+void Tstart()
+{
+	TimeBegin = clock();
+}
+
+void Tstop()
+{
+	TimeEnd = clock();
+	cout << "\nTime elapsed: " << 1000 * (TimeEnd - TimeBegin) / CLOCKS_PER_SEC << " ms\n";
 }
