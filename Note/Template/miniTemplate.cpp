23bba7eefeb3a0f747a83672cb4d2fb8f2c886d3
tncn1122 <39859635+tncn1122@users.noreply.github.com>
2019-05-02 15:52:07 +0700

    Update template

---
+++ Note/Template/miniTemplate.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define  Infile			freopen("input.txt", "r", stdin)
+#define  Outfile		freopen("output.txt", "w", stdout)
+
+typedef long long ll;
+typedef double db;
+
+//declare here
+
+//===========
+
+
+void Result()
+{
+	//code here
+	
+	
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	//Infile;
+	//Outfile;
+	Result();
+	
+	return 0;
+}
+
--- Note/Template/template.cpp
+++ Note/Template/template.cpp
@@ -1,32 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define  Infile         freopen("input.txt", "r", stdin)
-#define  Outfile        freopen("output.txt", "w", stdout)
-#define  fu(i,a,b)      for(long long i=(a); i<=(b); ++i)
-#define  fd(i,a,b)      for(long long i=(a); i>=(b); --i)
-#define  fo(i,n)        for(long long i=0; i<(n); ++i)
-#define  op(x)          cout << endl << x << endl
-#define  fset(x)        fixed<<setprecision(x)
-#define  ms(a,x)        memset(a, x, sizeof(a))
-#define  all(x)         (x).begin(), (x).end()
-#define	 fst            first
-#define	 scd            second
+#define  Infile			freopen("input.txt", "r", stdin)
+#define  Outfile		freopen("output.txt", "w", stdout)
+#define  lowb(a,x) 		lower_bound(a.begin(),a.end(),x)-a.begin();
+#define  upb(a,x) 		upper_bound(a.begin(),a.end(),x)-a.begin();
+#define  fu(i,a,b)		for(long long i=(a); i<=(b); ++i)
+#define  fd(i,a,b)		for(long long i=(a); i>=(b); --i)
+#define  fo(i,n)		for(long long i=0; i<(n); ++i)
+#define  show(a) 		cout<<"\n-> "<<#a<<" = "<<(a)<<endl;
+#define  fix_set(x)		fixed<<setprecision(x)
+#define  ms(a)			memset(a, 0, sizeof(a))
+#define  all(x)			(x).begin(), (x).end()
+#define  pb(x)			push_back(x)
+#define  ar(x, n)		(x),(x)+(n)
+#define  mp(a,b)		make_pair(a, b)
+#define	 fst			first
+#define	 snd			second
 
-typedef double db;
 typedef long long ll;
+typedef vector <ll> vtll;
+
 clock_t TimeBegin, TimeEnd;
-ll const INF = 1000000000000000000;
-ll const Base = 1000000007;
+ll LX[] = {-1,0,0,1,-1,-1,1,1}; // 8 - 4
+ll LY[] = {0,-1,1,0,-1,1,-1,1}; 
+ll const INF = LLONG_MAX;
+
 
 inline ll mod(string num, ll Base);
 inline ll diMod(ll A, ll B, ll Base);
 inline ll fpow(ll a, ll x, ll Base);
+inline string toString(ll n);
 inline void Tstart();
 inline void Tstop();
 
 
-//declare
+//Declare Here
 
 
 //============//
@@ -41,11 +50,13 @@ void Solve()
 
 int main()
 {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr); 
-	//Infile;
-	//Outfile;
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
-    Solve();
+	#ifdef RAVENCLAW
+		Infile;
+		//Outfile;
+	#endif
+		Solve();
 
 	return 0;
 }
@@ -63,32 +74,38 @@ inline ll mod(string num, ll Base)
 
 inline ll fpow(ll a, ll x, ll Base)
 {
-    if(x==0)return 1;	
-    if(x&1) 
-    {
-        return a*fpow(a,x-1)%Base;
-    } 
-    else
-    {
-        ll t=fpow(a,x/2);
-        return t*t%Base;
-    }
+	if(x==0)return 1;	
+	if(x&1) 
+	{
+		return a*fpow(a, x-1, Base)%Base;
+	} 
+	else
+	{
+		ll t=fpow(a, x/2, Base);
+		return t*t%Base;
+	}
 }
 
 inline ll diMod(ll A, ll B, ll Base)
 {
-    //(A/B)%Base
-    ll result = (A * fpow(B, Base-2)) % Base; 
-    return result;
+	//(A/B)%Base
+	ll result = (A * fpow(B, Base-2, Base)) % Base; 
+	return result;
+}
+
+inline string toString(ll n) 
+{
+	stringstream ss; ss << n;
+	return ss.str();
 }	
 
 inline void Tstart()
 {
-    TimeBegin = clock();
+	TimeBegin = clock();
 }
 
 inline void Tstop()
 {
-    TimeEnd = clock();
-    cout << "\nTime elapsed: " << 1000 * (TimeEnd - TimeBegin) / CLOCKS_PER_SEC << " ms\n";
+	TimeEnd = clock();
+	cout << "\nTime elapsed: " << 1000 * (TimeEnd - TimeBegin) / CLOCKS_PER_SEC << " ms\n";
 }
