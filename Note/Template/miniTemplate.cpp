674290a8f252c75a4c2b049e69e4ed8af32b6543
tncn1122 <39859635+tncn1122@users.noreply.github.com>
2019-06-03 13:41:42 +0700

    update minitmp

--- Note/Template/miniTemplate.cpp
+++ Note/Template/miniTemplate.cpp
@@ -22,8 +22,10 @@ void Result()
 int main()
 {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	//Infile;
-	//Outfile;
+	#ifdef RIMURU
+		Infile;
+		Outfile;
+	#enif
 	Result();
 	
 	return 0;
